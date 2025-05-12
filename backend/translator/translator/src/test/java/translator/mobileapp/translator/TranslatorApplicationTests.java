6496f5096a4700a9b51abd27006c2aa50804a1f1
Sebastian Rodriguez <sebrod35@purdue.edu>
2025-05-12 00:04:15 -0400

    backend stuff

---
+++ backend/translator/translator/src/test/java/translator/mobileapp/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package translator.mobileapp.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
